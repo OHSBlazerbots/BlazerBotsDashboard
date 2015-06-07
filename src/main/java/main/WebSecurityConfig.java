a456d3d8c845d1645ef53b1b9e1b9097c159ffb2
sgoldman <sgoldman216@gmail.com>
2015-06-07 15:52:34 -0600

    Security Stuff

--- src/main/java/main/WebSecurityConfig.java
+++ src/main/java/main/WebSecurityConfig.java
@@ -1,7 +1,9 @@
 package main;
 
+import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.encoding.ShaPasswordEncoder;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,6 +12,10 @@ import org.springframework.security.config.annotation.web.servlet.configuration.
 @Configuration
 @EnableWebMvcSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    
+    @Autowired
+    DataSource dataSource;
+    
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -27,8 +33,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-            .inMemoryAuthentication()
-                .withUser("user").password("password").roles("USER");
+        CustomPasswordEncoder cpe = new CustomPasswordEncoder("test");
+        
+        auth.jdbcAuthentication().dataSource(dataSource).usersByUsernameQuery("SELECT * FROM people WHERE username = ?").passwordEncoder(cpe);
+        System.out.println("Password: " + cpe.encodePassword("password", "test"));
     }
 }
+
+/**
+PPassword: 468faff8392f2576dbf6605a1b369c1b08fb89991
+*          468faff8392f2576dbf6605a1b369c1b08fb8999
+ */
