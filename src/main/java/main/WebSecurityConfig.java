dfd3cd1917ba7802feb20f35e83c971367a78447
sgoldman <sgoldman216@gmail.com>
2015-07-05 21:52:03 -0600

    Moved User and Carrier and their repositories. Removed the custom password encoder (wasn't proper convention, use userdetailsservice instead) and made corresponding edits to WebSecurityConfig.

--- src/main/java/main/WebSecurityConfig.java
+++ src/main/java/main/WebSecurityConfig.java
@@ -1,6 +1,10 @@
 package main;
 
+import main.repositories.UserRepository;
+import main.repositories.CarrierRepository;
 import javax.sql.DataSource;
+import main.models.Carrier;
+import main.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.encoding.ShaPasswordEncoder;
@@ -12,29 +16,34 @@ import org.springframework.security.config.annotation.web.servlet.configuration.
 @Configuration
 @EnableWebMvcSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    
+
     @Autowired
     DataSource dataSource;
-    
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    CarrierRepository carrierRepository;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-            .authorizeRequests()
+                .authorizeRequests()
                 .antMatchers("/", "/home").permitAll()
                 .anyRequest().authenticated()
                 .and()
-            .formLogin()
+                .formLogin()
                 .loginPage("/login")
                 .permitAll()
                 .and()
-            .logout()
+                .logout()
                 .permitAll();
     }
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        CustomPasswordEncoder cpe = new CustomPasswordEncoder("test");
-        
-        auth.jdbcAuthentication().dataSource(dataSource).usersByUsernameQuery("SELECT * FROM people WHERE username = ?").passwordEncoder(cpe);
+
+        auth.jdbcAuthentication().dataSource(dataSource).usersByUsernameQuery("SELECT * FROM users WHERE username = ?");//.passwordEncoder(cpe);
     }
 }
