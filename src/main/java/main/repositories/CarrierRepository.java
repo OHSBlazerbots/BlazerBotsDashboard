8b358f28fce33378ad8984a10482ef209393b024
sgoldman <sgoldman216@gmail.com>
2015-07-05 21:52:26 -0600

    Moved repositories. No edits.

---
+++ src/main/java/main/repositories/CarrierRepository.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main.repositories;
+
+import main.models.Carrier;
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author Sam
+ */
+public interface CarrierRepository extends CrudRepository<Carrier, Long>{
+    List<Carrier> findByName(String name);
+}
